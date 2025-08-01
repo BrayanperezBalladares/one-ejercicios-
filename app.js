a5c21a5cc57213b228ab62ff0a628ea85322040e
Brayan <bpballadares57@gmail.com>
2025-08-01 01:44:17 -0600

    Agrego ejercicios solicitados sobre creación de funciones para controlar eventos del usuario cuando interactua con botones

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+let titulo = document.querySelector("h1");
+titulo.textContent = "Hora del Desafío";
 
+function botonConsole() {
+    alert("El boton fue clicado");
+}
+
+function botonPrompt() {
+    
+    let nombreCiudad = prompt("Dime una ciudad de Brasil");
+    alert(`Estuve en ${nombreCiudad} y me he acordado de ti`)
+}
+function botonAlert() {
+    alert("Yo amo JS?")
+}
+function botonSuma() {
+    let numero1 = parseInt(prompt("Dime un número"));
+    let numero2 = parseInt(prompt("Dime otro número"));
+    alert(`La suma de ${numero1} y ${numero2} es ${numero1 + numero2}`);
+}
